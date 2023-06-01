a45115b211861540ab09f785f8c40ef9a54c8a07
Uzair Shahzad <u.shahzad+github@tarabutgateway.com>
2023-06-01 17:31:27 +0400

    Added the framework

---
+++ Frameworks/TGMerchantSDK.xcframework/ios-arm64/TGMerchantSDK.framework/Headers/TGMerchantSDK.h
@@ -0,0 +1,18 @@
+//
+//  TGMerchantSDK.h
+//  TGMerchantSDK
+//
+//  Created by Uzair Shahzad on 02/05/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TGMerchantSDK.
+FOUNDATION_EXPORT double TGMerchantSDKVersionNumber;
+
+//! Project version string for TGMerchantSDK.
+FOUNDATION_EXPORT const unsigned char TGMerchantSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TGMerchantSDK/PublicHeader.h>
+
+
